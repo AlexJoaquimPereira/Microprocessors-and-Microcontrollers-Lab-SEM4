94c13ab0b3c983f73b16da00dd8f622a8939482d
Alex Joaquim Pereira <alexpereira1781@gmail.com>
2024-02-12 22:56:51 +0530

    Prints 9 stars using times directive Edited the code to make it correctly running

--- Experiment 2/c.asm
+++ Experiment 2/c.asm
@@ -1,6 +1,7 @@
 section .data
 stars times 9 db '*'
-starslen equ $-stars
+newline db 10, 0
+starlen equ 9
 
 section .text
 global _start
@@ -9,7 +10,13 @@ _start:
 	mov eax, 4
 	mov ebx, 1
 	mov ecx, stars
-	mov edx, starslen
+	mov edx, starlen
+	int 80h
+	
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, newline
+	mov edx, 1
 	int 80h
 
 mov eax, 1
