5a27371b887b4827b073752f537bf98786c70d44
Alex Joaquim Pereira <alexpereira1781@gmail.com>
2024-02-12 23:02:27 +0530

    Prints two strings using equ directive Completed the code

--- Experiment 2/d.asm
+++ Experiment 2/d.asm
@@ -1,8 +1,29 @@
-string1 EQU 'Hello World', 10
-string2 EQU 'I am programming', 10
-
 section .data
-s1 BYTE string1
-s1 BYTE string2
+string1 db 'Hello World', 10
+s1len equ $-string1
+string2 db 'I am programming', 10
+s2len equ $-string2
+sys.write equ 4
+sys.call equ 1
+sys.exit equ 1
+sys.restart equ
 
+section .text
+global _start
 
+_start:
+	mov eax, sys.write
+	mov ebx, sys.call
+	mov ecx, string1
+	mov edx, s1len
+	int 80h
+
+	mov eax, sys.write
+	mov ebx, sys.call
+	mov ecx, string2
+	mov edx, s2len
+	int 80h
+	
+mov eax, sys.exit
+mov ebx, sys.restart
+int 80h
