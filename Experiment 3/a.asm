86185446b2dd899fdc37a2b9c60e792fed4e9763
Alex Joaquim Pereira <alexpereira1781@gmail.com>
2024-03-08 11:57:34 +0530

    Add two numbers and display the sum using EQU directive Added the EQU directives

--- Experiment 3/a.asm
+++ Experiment 3/a.asm
@@ -3,6 +3,12 @@ s1 db 'Enter two numbers: '
 s1len equ $-s1
 s2 db 'The sum is: '
 s2len equ $-s2
+sys.write equ 4
+sys.read equ 3
+sys.readcall equ 2
+sys.call equ 1
+sys.exit equ 1
+sys.restart equ 0
 
 section .bss
 num1 resb 4
@@ -12,45 +18,44 @@ section .text
 global _start
 
 _start:
-    MOV EAX, 4
-    MOV EBX, 1
+    MOV EAX, sys.write
+    MOV EBX, sys.call
     MOV ECX, s1
     MOV EDX, s1len
     int 0x80
 
-    MOV EAX, 3
-    MOV EBX, 2
+    MOV EAX, sys.read
+    MOV EBX, sys.readcall
     MOV ECX, num1
     MOV EDX, 4
     int 0x80
 
-    MOV EAX, 3
-    MOV EBX, 2
+    MOV EAX, sys.read
+    MOV EBX, sys.call
     MOV ECX, num2
     MOV EDX, 4
     int 0x80
 
-    ;If we use AX, BX registers the garbage values don't arise it seems
-    MOV EAX, [num1]; We can use atmost one mem location
-    SUB EAX, '0'; to convert ASCII to decimal
-    MOV EBX, [num2]; stores addr of num2 to EBX
-    SUB EBX, '0'
-    ADD EAX, EBX; performs actual addition on the operands
-    SUB EAX, '0'
-    MOV [num1], EAX; moves value in EAX to num1
+    MOV AL, [num1]; We can use atmost one mem location
+    SUB AL, '0'; to convert ASCII to decimal
+    MOV BL, [num2]; stores addr of num2 to EBX
+    SUB BL, '0'
+    ADD AL, BL; performs actual addition on the operands
+    ADD AL, '0'
+    MOV [num1], AL; moves value in EAX to num1
 
-    MOV EAX, 4
-    MOV EBX, 1
+    MOV EAX, sys.write
+    MOV EBX, sys.call
     MOV ECX, s2
     MOV EDX, s2len
     int 0x80
 
-    MOV EAX, 4
-    MOV EBX, 1
+    MOV EAX, sys.write
+    MOV EBX, sys.call
     MOV ECX, num1
     MOV EDX, 4
     int 0x80
 
-MOV EAX, 1
-MOV EBX, 0
+MOV EAX, sys.exit
+MOV EBX, sys.restart
 int 0x80
