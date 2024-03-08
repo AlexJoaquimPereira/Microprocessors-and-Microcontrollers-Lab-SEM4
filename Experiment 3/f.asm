d778a810f3dd4175cdcf9d0f6af44eb54db0b35b
Alex Joaquim Pereira <alexpereira1781@gmail.com>
2024-03-08 12:12:15 +0530

    Display all odd numbers from 0 to 9 Aaditya-Salgaonkar 's code as I didn't have time to perfect it

--- Experiment 3/f.asm
+++ Experiment 3/f.asm
@@ -1,86 +1,94 @@
-;write ALP to display odd numbers from 0 to 9 without looping
-section .bss
-num resb 4
-
+section .data
+odd db 'All odd numbers from 0 to 9 are: ', 10
+oddlen equ $-odd
+n db '1'
+newline db 10, 0
 section .text
-global _start
-
+global _start:
 _start:
-    ;initializing to 1
-    MOV EAX, [num]
-    MOV EAX, 1
-    SUB EAX, '0'
-    MOV [num], EAX
 
-    ;Incrementing and printing the odd numbers four times
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, num
-    MOV EDX, 4
-    int 0x80
-    INC [num]
-    INC [num]
+	mov eax,4
+	mov ebx,1
+	mov ecx,odd
+	mov edx,oddlen
+	int 80h
 
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, 10
-    MOV EDX, 0
-    int 0x80
+	mov eax,4
+	mov ebx,1
+	mov ecx,n
+	mov edx,1
+	int 80h
 
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, num
-    MOV EDX, 4
-    int 0x80
-    INC [num]
-    INC [num]
+	mov eax,4
+	mov ebx,1
+	mov ecx,newline
+	mov edx,1
+	int 80h
 
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, 10
-    MOV EDX, 0
-    int 0x80
+	mov eax,[n]
+	inc eax
+	inc eax
+	mov[n],eax
+	mov eax,4
+	mov ebx,1
+	mov ecx,n
+	mov edx,1
+	int 80h
 
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, num
-    MOV EDX, 4
-    int 0x80
-    INC [num]
-    INC [num]
-    
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, 10
-    MOV EDX, 0
-    int 0x80
+	mov eax,4
+	mov ebx,1
+	mov ecx,newline
+	mov edx,1
+	int 80h
 
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, num
-    MOV EDX, 4
-    int 0x80
-    INC [num]
-    INC [num]
-    
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, 10
-    MOV EDX, 0
-    int 0x80
+	mov eax,[n]
+	inc eax
+	inc eax
+	mov[n],eax
+	mov eax,4
+	mov ebx,1
+	mov ecx,n
+	mov edx,1
+	int 80h
 
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, num
-    MOV EDX, 4
-    int 0x80
+	mov eax,4
+	mov ebx,1
+	mov ecx,newline
+	mov edx,1
+	int 80h
 
-    MOV EAX, 4
-    MOV EBX, 1
-    MOV ECX, 10
-    MOV EDX, 0
-    int 0x80
+	mov eax,[n]
+	inc eax
+	inc eax
+	mov[n],eax
+	mov eax,4
+	mov ebx,1
+	mov ecx,n
+	mov edx,1
+	int 80h
 
-MOV EAX, 1
-MOV EBX, 0
-int 0x80
+	mov eax,4
+	mov ebx,1
+	mov ecx,newline
+	mov edx,1
+	int 80h
+
+	mov eax,[n]
+	inc eax
+	inc eax
+	mov[n],eax
+	mov eax,4
+	mov ebx,1
+	mov ecx,n
+	mov edx,1
+	int 80h
+
+	mov eax,4
+	mov ebx,1
+	mov ecx,newline
+	mov edx,1
+	int 80h
+
+mov eax,1
+mov ebx,0
+int 80h
