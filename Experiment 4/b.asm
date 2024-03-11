1320b7f5cf775863fa270d4597261767d4834993
Alex Joaquim Pereira <alexpereira1781@gmail.com>
2024-03-11 21:56:31 +0530

    Finds the largest of 3 numbers Done some part in the lab, don't know if it works

---
+++ Experiment 4/b.asm
@@ -0,0 +1,97 @@
+;Write an assembly language program to find the largest of three numbers.
+section .data
+str db 'Enter three numbers: '
+strlen equ $-str
+largest db 'The largest number is: '
+largestlen equ $-largest
+
+section .bss
+num1 resb 1
+num2 resb 1
+num3 resb 1
+
+section .text
+global _start
+
+_start:
+    ;enter three numbers
+    MOV EAX, 4
+    MOV EBX, 1
+    MOV ECX, str
+    MOV EDX, strlen
+    int 0x80
+
+    MOV EAX, 3
+    MOV EBX, 2
+    MOV ECX, num1
+    MOV EDX, 1
+    int 0x80
+
+    MOV EAX, 3
+    MOV EBX, 2
+    MOV ECX, num2
+    MOV EDX, 1
+    int 0x80
+
+    MOV EAX, 3
+    MOV EBX, 2
+    MOV ECX, num3
+    MOV EDX, 1
+    int 0x80
+
+    MOV EAX, 4
+    MOV EBX, 1
+    MOV ECX, largest
+    MOV EDX, largestlen
+    int 0x80
+
+    ;compare num1 and num2
+    MOV EAX, [num1]
+    MOV EBX, [num2]
+    CMP EAX, EBX
+    JG COMPARE_1_3; num1 > num2 then test num1 > num3
+    JL COMPARE_2_3; num1 < num2 then test num2 < num3
+
+    COMPARE_1_3:
+    MOV EAX, [num1]
+    MOV EBX, [num3]
+    CMP EAX, EBX
+    JG PRINT_1; if num1 > num3
+    JL PRINT_3; if num1 < num3
+
+    COMPARE_2_3:
+    MOV EAX, [num2]
+    MOV EBX, [num3]
+    CMP EAX, EBX
+    JG PRINT_2;if num2 > num3
+    JL PRINT_3;if num2 < num3
+
+    PRINT_1:
+    MOV EAX, 4
+    MOV EBX, 1
+    MOV ECX, num1
+    MOV EDX, 1
+    int 0x80
+    JMP END
+
+    PRINT_2:
+    MOV EAX, 4
+    MOV EBX, 1
+    MOV ECX, num2
+    MOV EDX, 1
+    int 0x80
+    JMP END
+
+    PRINT_3:
+    MOV EAX, 4
+    MOV EBX, 1
+    MOV ECX, num3
+    MOV EDX, 1
+    int 0x80
+    JMP END
+
+END:
+
+MOV EAX, 1
+MOV EBX, 0
+int 0x80
