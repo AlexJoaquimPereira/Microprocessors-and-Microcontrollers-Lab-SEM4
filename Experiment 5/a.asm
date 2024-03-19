49dfc75393fb9a33462f73f659a65142c5740594
Alex Joaquim Pereira <alexpereira1781@gmail.com>
2024-03-19 23:07:31 +0530

    Write system call using macros to display a number Changes done in lab

--- Experiment 5/a.asm
+++ Experiment 5/a.asm
@@ -1,8 +1,8 @@
 %MACRO write_string 2
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, %1
-    mov edx, %2
+    MOV EAX, 4
+    MOV EBX, 1
+    MOV ECX, %1
+    MOV EDX, %2
     int 0x80
 %ENDMACRO
 
@@ -20,16 +20,16 @@ global _start
 _start:
     write_string s1, s1l
 
-	mov eax, 3
-	mov ebx, 2
-	mov ecx, num
-	mov edx, 4
+	MOV EAX, 3
+	MOV EBX, 2
+	MOV ECX, num
+	MOV EDX, 4
 	int 0x80
 
 	write_string s2, s2l
 	write_string num, 4
     write_string 10, 1
 
-mov eax, 1
-mov ebx, 0
+MOV EAX, 1
+MOV EBX, 0
 int 80h
