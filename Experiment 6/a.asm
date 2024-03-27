f6dd3857a1f4f97bb1ccea2513f1c8039f807a48
Alex Joaquim Pereira <alexpereira1781@gmail.com>
2024-03-27 16:47:02 +0530

    Fibonacci series using procedures Used Deeptej's code. Although my idea of recursive program was good it showed segmentation fault

--- Experiment 6/a.asm
+++ Experiment 6/a.asm
@@ -1,84 +1,105 @@
-%macro write_string 2
-	MOV EAX, 4
-	MOV EBX, 1
-    PUSH ECX
-	MOV ECX, %1
-	MOV EDX, %2
-	int 80h
-    POP ECX
-%endmacro
+;fibonacci series
 
-%macro read_string 2
-	MOV EAX, 3
-	MOV EBX, 2
-	MOV ECX, %1
-	MOV EDX, %2
-	int 80h
-%endmacro
+%MACRO read_string 2
+    MOV EAX, 3
+    MOV EBX, 2
+    MOV ECX, %1
+    MOV EDX, %2
+    int 0x80
+%ENDMACRO
+
+%MACRO write_string 2
+    MOV EAX, 4
+    MOV EBX, 1
+    MOV ECX, %1
+    MOV EDX, %2
+    int 0x80
+%ENDMACRO
 
 section .data
-	ask db 'Enter the limit: '
-	asklen equ $-ask
-	show db 'Fibonacci series: '
-	showlen equ $-show
-	space db ' '
-	spacelen equ $-space
-	newline db 10
+    newline db 10
+
+    ask db 'Enter the value of n: '
+    asklen equ $-ask 
+
+    show db 'Fibonacci sequence: '
+    showlen equ $-show
+
+    space db ' '
+    spacelen equ $-space
 
 section .bss
-	i resb 1
-	n resb 4
-	a resb 4
-	b resb 4
-	c resb 4
+    i resb 1
+    n resb 1
+    a resb 1
+    b resb 1
+    c resb 1
 
 
 section .text
-	global _start
+    global _start
+
 _start:
+    write_string ask, asklen
+    read_string n, 1
+    write_string show, showlen
 
-	write_string ask, asklen
-	read_string n, 4
-	write_string show, showlen
+    CALL fib
+    write_string newline, 10
 
-	MOV [i], byte '0'
-	MOV [a], byte '0'
-	MOV [b], byte '1'
+    MOV EAX, 1
+    MOV EBX, 0
+    int 0x80
 
-    ;push function parameters to stack
-    MOV EAX, [n]
-    MOV EDX, [b]
-    MOV ECX, [a]
-    PUSH ECX
-    PUSH EDX
-    PUSH EAX
-    CALL recursive_fibonacci
-    CALL exit
+formula:
+    MOV AL, [a]
+    MOV BL, [b]
+    sub AL, '0'
+    sub BL, '0'
+    add AL, BL
+    add AL, '0'
+    MOV [c], AL
+RET
 
-;normal_fibonacci 
+fib:
+    MOV [i], byte '0'
+    MOV [a], byte '0'
+    MOV [b], byte '1'
 
-recursive_fibonacci: ;recursion gives segmentation fault
-    POP EAX
-    POP EDX
-    POP ECX
-    CMP ECX, '0'
-    JE exit ; if n == 0 return
-    write_string EAX, 4; else print
-    write_string 10, 1
-    DEC ECX
-    PUSH EDX
-    SUB EAX, '0'
-    SUB EDX, '0'
-    ADD EDX, EAX    ;b = b + a
-    SUB EDX, '0'
-    POP EAX         ; a = b
-    PUSH ECX
-    PUSH EDX
-    PUSH EAX
-    CALL recursive_fibonacci
+    MOV AL, [n]
+    CMP AL, '0'
+    JE exit
 
-exit:
-	write_string newline, 1
-	MOV EAX, 1
-	MOV EBX, 0
-	int 80h
+    write_string a, 1
+    write_string space, spacelen
+    INC byte[i]
+
+    MOV AL, [i]
+    CMP AL, byte[n]
+    JE exit
+
+    write_string b, 1
+    write_string space, spacelen
+
+    INC byte[i]
+    MOV AL, [i]
+    CMP AL, byte[n]
+    JE exit
+
+    loop:
+        CALL formula
+        write_string c, 1
+        write_string space, spacelen
+
+        MOV AL, [b]
+        MOV [a], AL
+        MOV AL, [c]
+        MOV [b], AL
+
+        INC byte[i]
+        MOV AL, [i]
+        CMP AL, byte[n]
+        JE exit
+        JMP loop
+    exit:
+RET
